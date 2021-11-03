5d8e46d437d392a36e2343e861b31ad201ab6d04
zdurr22 <zdurr22@gmail.com>
2021-11-03 09:54:01 -0400

    commit tree 2db46cd2a6d1789a83ad591739b09de6e6ed1457

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      <NavLink to="/" exact style={link}>Home</NavLink>
-      <NavLink to="/movies" exact style={link}>Movies</NavLink>
-      <NavLink to="/directors" exact style={link}>Directors</NavLink>
-      <NavLink to="/actors" exact style={link}>Actors</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
     </div>
   );
 };
